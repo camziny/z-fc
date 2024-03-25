264cad291caa71b35d2d5a3342f7f36c0b4917de
camziny <cameronziny@gmail.com>
2024-03-25 19:40:29 -0400

    - begin player detail page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'home/index'
   get 'users/profile', to: 'users#profile', as: :user_profile
+  get 'players/:id', to: 'players#show', as: 'player'
 
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
